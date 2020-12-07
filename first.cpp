48a844ef50d76fae4048189b619e26cc4476c628
manvi0504 <75639822+manvi0504@users.noreply.github.com>
2020-12-08 02:43:29 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,35 +1,35 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-leftRotatebyOne(int arr,int n)
+void leftRotatebyOne(int arr[],int n)
 {
-    int temp = arr[0];
-    for(int i = 0;i<n-1;i++)
-        arr[i]=arr[i+1];
-    
+    int temp = arr[0],i;
+    for(i=0;i<n-1;i++)
+    arr[i]=arr[i+1];
     arr[i]=temp;
 }
 
-
 void leftRotate(int arr[],int d,int n)
 {
-    for(int i = 0;i<d;i++)
-    leftRotatebyOne(arr,n);
+    for(int i = 0; i < d; i++)
+    leftRotatebyOne( arr, n);
 }
 
-void printArray(int arr[], int n) 
-{ 
-    for (int i = 0; i < n; i++) 
-        cout << arr[i] << " "; 
-} 
+void printArray(int arr[], int n)
+{
+    for(int i = 0;i < n;i++)
+    cout<<arr[i];
+}
 
 int main()
 {
     int arr[] = {1,2,3,4,5,6,7};
     int n = sizeof(arr)/sizeof(arr[0]);
     
-    leftRotate(arr,2,n);
-    printArray(arr,n);
+    leftRotate( arr, 2, n);
+    printArray( arr,n);
     
     return 0;
+    
 }
